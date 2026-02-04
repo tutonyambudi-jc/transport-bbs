9f2f0dc229d76ebb7acc04f05f5b74b72125731a
big business solutions <deploy@vmi3039946.contaboserver.net>
2026-02-04 14:42:32 +0100

    Fix: PORT 3001 + PostgreSQL schema

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
     exec_mode: 'cluster',
     env: {
       NODE_ENV: 'production',
-      PORT: 3000,
+      PORT: 3001,
     },
     error_file: './logs/pm2-error.log',
     out_file: './logs/pm2-out.log',
